6afa45b55cde608bf90a59090427bc9b336bc558
devraj <draj56699@gmail.com>
2022-07-28 20:17:29 +0530

    "Added Callback function"

--- Ch01/01_01/start/index.js
+++ Ch01/01_01/start/index.js
@@ -1,7 +1,22 @@
-function hideString(str) {
-    return str.replace(/[a-zA-Z]/g, 'X');
-}
+// function hideString(str) {
+//     return str.replace(/[a-zA-Z]/g, 'X');
+// }
 
-var hidden = hideString("Hello World");
+// var hidden = hideString("Hello World");
 
-console.log( hidden );
+// console.log( hidden );
+// console.log("End");
+
+// --------------------------------------------------------------------
+
+// function hideString(str,done) {
+//     process.nextTick(() => {
+//         done(str.replace(/[a-zA-Z]/g, 'X'));
+//     });
+// }
+
+// hideString("hello World",(hidden) => {
+//     console.log(hidden);
+// })
+
+// console.log("End");
